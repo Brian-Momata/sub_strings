cfc75fab4421464b01345ff0050dc478d9c33de3
Brian-Momata <brianmomata@gmail.com>
2022-08-29 11:01:57 +0300

    Add a dictonary array and illustration

--- substring.rb
+++ substring.rb
@@ -6,3 +6,5 @@ def sub_strings(string, dictionary)
     hash
   end 
 end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+sub_strings("Howdy partner, sit down! How's it going?", dictionary)
