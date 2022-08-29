b929b0c8fb9d8872fa81bb3a532d00f81a2722d4
Brian-Momata <brianmomata@gmail.com>
2022-08-29 10:57:50 +0300

    Fix method to handle multiple words

--- substring.rb
+++ substring.rb
@@ -1,6 +1,8 @@
 def sub_strings(string, dictionary)
   dictionary.reduce(Hash.new(0)) do |hash, word|
-    hash[word] += 1 if string.downcase.include?(word.downcase)
+    string.split(" ").each do |element|
+      hash[word] += 1 if element.downcase.include?(word.downcase)
+    end
     hash
   end 
 end
