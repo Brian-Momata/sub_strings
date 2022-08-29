c0e68f2411d50f9f75729717f738a37df8b022a8
Brian-Momata <brianmomata@gmail.com>
2022-08-29 09:35:06 +0300

    Fix to be case insensitive

--- substring.rb
+++ substring.rb
@@ -1,6 +1,6 @@
 def sub_strings(string, dictionary)
   dictionary.reduce(Hash.new(0)) do |hash, word|
-    hash[word] += 1 if string.include?(word)
+    hash[word] += 1 if string.downcase.include?(word.downcase)
     hash
   end 
 end
